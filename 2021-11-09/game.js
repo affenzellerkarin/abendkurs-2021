4dd8084cafee97f5870165934b848a943d7639b2
Rebecca <office@rebecca-rottensteiner.at>
2021-11-08 14:41:47 +0100

    Kommentare

--- 2021-11-09/game.js
+++ 2021-11-09/game.js
@@ -1,29 +1,70 @@
 "use strict";
 
 // ToDo Variablen Deklarieren
+let a, b, c, sum, question, user, tip, userResult;
+let level = 1;
 
 // ToDo Buttons ausblenden und deaktivieren
 
 // ToDo 
 function username() {
+  // Namen des Spielers abfragen und in eine Variable speichern
+
+  // Funktion aufrufen um den richtigen Button zu aktivieren
+
+  // Play Button verstecken
+
+  // Restart Button einblenden
 
 }
 
 // ToDo 
 function activeButton(id) {
+  // Button klickbar machen
+
+  // Button die Klasse active zufügen
+
+  // vorherigen Button deaktivieren
+
+  // vorherigen Button Klasse active entfernen
+
+  // Game mit Level aufrufen
 
 }
 
 // ToDo 
 function start() {
+  // input leeren
+
+  // Seite neu laden
 
 }
 
 // ToDo 
 function checkResult() {
+  // Input auslesen
+
+  // Überprüfen ob Userergebnis richtig ist
+
+    // wenn ja dann Ausgabe gut
+
+    // Input Feld wieder leeren
+
+    // Level rauf zählen
+
+    // nächsten Button aktivieren
   
+  // ansonsten
+
+    // Ausgabe falsch
+
+  // Wenn level 9 erreicht
+
+    // FadeOut
+
 }
 
+// Die Aufgaben in den Runden
 function game(num) {
   switch (num) {
     case 1:
@@ -83,5 +124,7 @@ function game(num) {
       sum = Math.pow(a, b);
       break;
   }
-  // ToDo
+  console.log(sum);
+  // ToDo Ausgabe der Frage 
+
 }
